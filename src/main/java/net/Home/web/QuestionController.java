80391c97412caecc6eedaf34ce4e7d7393d8bd90
bok-PC\bok <bok_0120@naver.com>
2018-06-06 17:13:59 +0900

    질문 상세보기 기능

--- src/main/java/net/Home/web/QuestionController.java
+++ src/main/java/net/Home/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -46,4 +48,14 @@ public class QuestionController {
 		questionRepository.save(question);
 		return "redirect:/";
 	}
+	
+	@GetMapping("/{Id}/form")
+	public String show(@PathVariable Long Id, Model model) {
+		
+		Question question = questionRepository.findOne(Id);
+		model.addAttribute("question", question);
+		return "/qna/show";
+	}
+	
+	
 }
